6c649dd8b753822f3160cd361001162ffbf65ceb
BEaStia <gophan1992@gmail.com>
2015-02-12 00:14:03 +0300

    created client

--- db/migrate/20150130201115_create_models.rb
+++
@@ -1,9 +0,0 @@
-class CreateModels < ActiveRecord::Migration
-  def change
-    create_table :models do |t|
-      t.belongs_to :maker, index: true
-      t.text :model
-      t.integer :autoru
-    end
-  end
-end
